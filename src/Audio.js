ae147e13fce1d2d3aa36d48ac3b975ebe8c9c78a
ten <ten@mutate.space>
2018-09-27 22:08:56 +0200

    doggo now use high tech to bark your sound

--- src/Audio.js
+++ src/Audio.js
@@ -1,94 +1,110 @@
 import React from "react";
 import { TweenMax } from "gsap";
 import FileProcessor from "./FileProcessor.js";
-import dog_wav from "./sound/dog.wav";
 
 export default class Audio {
-  context = null;
-  $mediaElement = React.createRef();
   constructor() {
     const audio = this;
-    //
     this.reactComponent = class AudioComponent extends React.Component {
       static instance = null;
       constructor() {
         super();
         audio.reactComponent.instance = this;
       }
-      state = {
-        url: dog_wav
-      };
-      componentDidUpdate(previousProps, previousState) {
-        if (previousState.url !== this.state.url) {
-          URL.revokeObjectURL(previousState.url);
-        }
-      }
-      componentDidMount() {
-        audio.domElementReady = true;
-      }
       render() {
-        return (
-          <audio
-            ref={audio.$mediaElement}
-            src={this.state.url}
-            id="dog-sound"
-          />
-        );
+        return <div />;
       }
     };
   }
   //
+  context = null;
+  nodes = {};
   createAudioContext() {
-    if (!this.$mediaElement.current) {
-      console.warn("audio DOM element is not ready yet.");
-      return;
-    }
-    this.context = new AudioContext();
-    const source = this.context.createMediaElementSource(
-      this.$mediaElement.current
-    );
+    this.context = new (window.AudioContext || window.webkitAudioContext)();
     const analyser = this.context.createAnalyser();
     const gain = this.context.createGain();
     const destination = this.context.destination;
-    // Node Chain
-    [source, analyser, gain, destination].reduce((node, next) => {
+    [analyser, gain, destination].reduce((node, next) => {
       node.connect(next);
       return next;
     });
-    // Node references
     this.nodes = {
-      source,
       analyser,
       gain,
       destination
     };
   }
+  reconnectNodes() {
+    const { source, analyser, gain, destination } = this.nodes;
+    this.nodes = { source, analyser, gain, destination };
+  }
+  fileReader = new FileReader();
+  audioBuffer = null;
+  decodeAudioFile(file) {
+    this.fileReader.onload = () => {
+      const buffer = this.fileReader.result;
+      this.context.decodeAudioData(
+        buffer,
+        audioBuffer => {
+          this.audioBuffer = audioBuffer;
+          const durationInSeconds = audioBuffer.length / audioBuffer.sampleRate;
+        },
+        error => console.warn("Dog Audio: failed to decode audio data. ", error)
+      );
+    };
+    this.fileReader.readAsArrayBuffer(file);
+  }
   //
   // Dog Behaviours
   //
-  onFileProcess = () => {};
-  onFileStart = (file, type, processor) => {
+  onFileProcess = (file, type) => {
     if (type === FileProcessor.TYPE_AUDIO) {
-      this.reactComponent.instance.setState({
-        url: URL.createObjectURL(file)
-      });
-    }
-  };
-  bark(bool) {
-    if (this.$mediaElement.current) {
       if (this.context === null) {
         this.createAudioContext();
       }
-      if (bool) {
-        TweenMax.to(this.nodes.gain.gain, 0, {
-          value: 1
-        });
-        this.$mediaElement.current.currentTime = 0;
-        this.$mediaElement.current.play();
-      } else {
-        TweenMax.to(this.nodes.gain.gain, 0.2, {
-          value: 0
-        });
+      this.decodeAudioFile(file);
+    }
+  };
+  onFileStart = () => {};
+  startBufferSourceNode(node, fadeDuration = 0) {
+    return new Promise((resolve, reject) => {
+      node.start();
+      TweenMax.to(node.gain, fadeDuration, {
+        value: 1,
+        onComplete: resolve
+      });
+    });
+  }
+  stopBufferSourceNode(node, fadeDuration = 0.2) {
+    return new Promise((resolve, reject) => {
+      TweenMax.to(node.gain, fadeDuration, {
+        value: 0,
+        onComplete: () => {
+          try {
+            node.stop();
+          } catch (e) {
+            // dispose error
+          }
+          resolve();
+        }
+      });
+    });
+  }
+  bark(bool) {
+    if (this.context === null) {
+      this.createAudioContext();
+    }
+    if (bool) {
+      if (this.nodes.source) {
+        this.stopBufferSourceNode(this.nodes.source, 0.1);
+      }
+      this.nodes.source = this.context.createBufferSource();
+      this.nodes.source.buffer = this.audioBuffer;
+      this.nodes.source.connect(this.nodes.analyser);
+      this.startBufferSourceNode(this.nodes.source);
+    } else {
+      if (this.nodes.source) {
+        this.stopBufferSourceNode(this.nodes.source);
       }
     }
   }
