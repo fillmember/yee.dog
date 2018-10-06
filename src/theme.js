113edadb5d850bc904de211037fb4b66235ba44c
ten <ten@mutate.space>
2018-10-06 17:34:42 +0200

    update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,10 @@
 export default {
-  fontSizes: [8, 10, 12, 16, 20, 24, 32, 40],
-  space: [0, 4, 8, 16, 32, 64, 128, 256],
   fonts: {
-    sans: "system-ui, sans-serif",
-    mono: "Menlo, monospace"
+    sans: `"Comfortaa", cursive`,
+    mono: `"SF Mono", "Roboto Mono", Menlo, monospace`
   },
   colors: {
     white: "#F2F2F2",
     green: "#1B8547"
-  },
-  shadows: {
-    small: "0 0 4px rgba(0, 0, 0, .125)",
-    large: "0 0 24px rgba(0, 0, 0, .125)"
   }
 };
