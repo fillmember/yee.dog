df053abccbb40f917988523049b38bedaaa4ca28
ten <ten@mutate.space>
2018-11-26 07:16:15 +0100

    optimize Dog3D.js

--- src/3D/Dog3D.js
+++ src/3D/Dog3D.js
@@ -1,10 +1,5 @@
-import { MeshLambertMaterial, LinearEncoding, Math as Math3 } from "three";
-import { TweenMax, Power2 } from "gsap";
-import { DogIK } from "./IK.js";
+import { MeshLambertMaterial, LinearEncoding } from "three";
 import { Animation } from "./Animation.js";
-import BoneID from "./BoneID.js";
-import ParticleSystem from "./ParticleSystem.js";
-import { Clips, Actions } from "./AnimationData.js";
 
 export default class Dog3D {
   constructor({ obj3d, scene }) {
@@ -21,91 +16,8 @@ export default class Dog3D {
     mesh.material = mat;
     // Animation
     this.animation = new Animation(this.mesh);
-    // this.animation.makeClips(Clips);
-    // this.animation.makeActions(Actions);
-    // Particles
-    // this.particles = new ParticleSystem({
-    //   confused: {
-    //     max: 16,
-    //     parent: scene,
-    //     emitter: {
-    //       enabled: true,
-    //       rate: 0,
-    //       center: mesh.skeleton.bones[BoneID.Head],
-    //       extent: [1, 1, 1],
-    //       offset: [0, 1, 0],
-    //       velocity: [0, 0.01, 0],
-    //       lifespan: () => 1 + Math.random() * 2,
-    //       sprite: 37
-    //     }
-    //   },
-    //   surprise: {
-    //     max: 4,
-    //     parent: mesh.skeleton.bones[BoneID.Head],
-    //     emitter: {
-    //       enabled: true,
-    //       size: 2,
-    //       rate: 0,
-    //       center: [0, 180, 0],
-    //       lifespan: 0.8,
-    //       sprite: 36
-    //     }
-    //   },
-    //   digital: {
-    //     max: 512,
-    //     parent: scene,
-    //     emitter: {
-    //       enabled: true,
-    //       size: () => Math3.randFloat(0.4, 0.8),
-    //       rate: 0,
-    //       center: mesh.skeleton.bones[BoneID.Spine],
-    //       extent: [0.1, 0.1, 0.1],
-    //       sprite: [0, 1],
-    //       velocity: () => {
-    //         const a = 0.05;
-    //         return [
-    //           Math3.randFloat(-a, a),
-    //           Math3.randFloat(-a, a),
-    //           Math3.randFloat(-a, a)
-    //         ];
-    //       },
-    //       lifespan: () => Math3.randFloat(0.5, 2)
-    //     }
-    //   },
-    //   eat: {
-    //     max: 64,
-    //     parent: scene,
-    //     emitter: {
-    //       enabled: true,
-    //       size: 0.6,
-    //       rate: 0,
-    //       center: mesh.skeleton.bones[BoneID.JawU_1],
-    //       extend: [0.4, 0.1, 0.5],
-    //       offset: [0, -0.1, 0.9],
-    //       sprite: [0, 1],
-    //       velocity: () => [
-    //         Math.random() > 0.5 ? -0.03 : 0.03,
-    //         Math3.randFloat(-0.02, 0.02),
-    //         Math3.randFloat(-0.01, -0.05)
-    //       ],
-    //       acceleration: [0, -0.005, 0],
-    //       lifespan: 0.7
-    //     }
-    //   }
-    // });
   }
   update(dt) {
-    // this.particles.update(dt);
-    // this.ik.update(dt);
     this.animation.update(dt);
-  }
-  // Dog Behaviours
-  lookAt(vector3) {
-    // this.ik.chains.look && this.ik.chains.look.target.set(vector3);
-    // this.ik.chains.worm && this.ik.chains.worm.target.set(vector3);
-    // const action = this.animation.actions.vleg;
-    // TweenMax.to(action, 0.8, {
-    //   time: vector3.y < 0 ? 1 : 0
-    // });
   }
 }
