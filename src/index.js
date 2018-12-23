28a0c5f4fc90f8dc61fbd541217a536e8e8f1b47
ten <ten@mutate.space>
2018-12-23 17:02:41 +0100

    remove ServiceWorker & old index file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./css/reset.css";
-import "./css/basic.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
