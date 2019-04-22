91291431c4d00c867755ecc20bfa9e2614d7efd2
ten <ten@mutate.space>
2019-04-22 16:35:07 +0200

    chore: improve DoggoBehaviourReact
    - use PureComponent
    - implement componentDidCatch
    - try to create this.behaviour on render (ensure UI is available)

--- src/behaviours/DoggoBehaviourReact.js
+++ src/behaviours/DoggoBehaviourReact.js
@@ -2,38 +2,47 @@ import React from "react";
 import isEqual from "lodash/isEqual";
 import DogStore from "../DogStore";
 
-export default class DoggoBehaviourReact extends React.Component {
+export default class DoggoBehaviourReact extends React.PureComponent {
   behaviour = null;
+  state = {
+    error: null
+  };
   getArguments(obj = this.props) {
     const { value, ...others } = obj;
     return others;
   }
-  updateBehaviour() {
+  createDoggoBehaviour() {
+    const Behaviour = this.props.value;
+    this.behaviour = new Behaviour();
+    DogStore.addBehaviour(this.behaviour);
+  }
+  updateDoggoBehaviour() {
     const others = this.getArguments();
     const args = Object.keys(others);
     args.forEach(key => {
       this.behaviour.set(key, others[key]);
     });
   }
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+  }
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.value !== this.props.value) {
       console.warn("don't change behaviour on the fly!");
     }
     if (!isEqual(this.getArguments(prevProps), this.getArguments())) {
-      this.updateBehaviour();
+      this.updateDoggoBehaviour();
     }
-  }
-  componentDidMount() {
-    const Behaviour = this.props.value;
-    this.behaviour = new Behaviour();
-    this.updateBehaviour();
-    DogStore.addBehaviour(this.behaviour);
   }
   render() {
-    if (this.behaviour) {
-      return this.behaviour.renderUI();
+    if (this.state.error) {
+      return <pre>DoggoBehaviour Error</pre>;
     }
-    return false;
+    if (!this.behaviour) {
+      this.createDoggoBehaviour();
+      this.updateDoggoBehaviour();
+    }
+    return this.behaviour.renderUI();
   }
 }
 
