9f8c06f4ec20ba3b3e07c97ff837f74fbf86622e
ten <ten@mutate.space>
2019-07-14 09:57:58 +0200

    feat: make doggo more flexible (IK constraint angle larger)

--- src/data/ik-chains.js
+++ src/data/ik-chains.js
@@ -3,13 +3,13 @@ import BoneID from "../3D/BoneID";
 export default {
   worm: {
     joints: [BoneID.Spine, BoneID.Shoulder, BoneID.Neck, BoneID.Head],
-    constraints: [40, 15, 10, 5],
+    constraints: [90, 45, 25, 15],
     influence: 0.1,
     clipWeight: 0.5
   },
   look: {
     joints: [BoneID.Shoulder, BoneID.Neck, BoneID.Head],
-    constraints: [10, 40, 30],
+    constraints: [20, 85, 65],
     influence: 0.1,
     clipWeight: 1
   }
