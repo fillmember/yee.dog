99e6d09bae6cb5924176d5988e8127f70ebb3417
ten <ten@mutate.space>
2019-07-14 10:02:58 +0200

    fix: dog was too soft

--- src/data/ik-chains.js
+++ src/data/ik-chains.js
@@ -3,13 +3,13 @@ import BoneID from "../3D/BoneID";
 export default {
   worm: {
     joints: [BoneID.Spine, BoneID.Shoulder, BoneID.Neck, BoneID.Head],
-    constraints: [90, 45, 25, 15],
+    constraints: [40, 15, 10, 5],
     influence: 0.1,
     clipWeight: 0.5
   },
   look: {
     joints: [BoneID.Shoulder, BoneID.Neck, BoneID.Head],
-    constraints: [20, 85, 65],
+    constraints: [10, 40, 30].map(v => v * 1.4),
     influence: 0.1,
     clipWeight: 1
   }
