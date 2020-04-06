01c1cb1752b8331c59b1521a0606ca726666f3e7
Ten <ten@mutate.space>
2020-04-06 16:00:34 +0200

    feat: improve DogEvent system
    subscribe return unsubscriber
    create typed emit function

--- src/fiber/DogEvent.ts
+++ src/fiber/DogEvent.ts
@@ -2,10 +2,15 @@ import { EventEmitter } from "events";
 
 export const emitter = new EventEmitter();
 
-export const subscribe = (event: string, listener) => {
-  emitter.on(event, listener);
-};
-
 export const unsubscribe = (event: string, listener) => {
   emitter.off(event, listener);
 };
+
+export const subscribe = (event: string, listener) => {
+  emitter.on(event, listener);
+  return () => unsubscribe(event, listener);
+};
+
+export function emit<T>(event: string, payload: T) {
+  emitter.emit(event, payload);
+}
