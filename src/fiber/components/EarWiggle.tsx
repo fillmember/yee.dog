f9fa1a7c0625838b1c925ddd82f795a3cbc60231
Ten <ten@mutate.space>
2020-04-06 15:38:04 +0200

    refactor: EarWiggle

--- src/fiber/components/EarWiggle.tsx
+++ src/fiber/components/EarWiggle.tsx
@@ -3,19 +3,25 @@ import { useFrame } from "react-three-fiber";
 import { useDogBones } from "../hooks/useDogBone";
 import { Wiggle } from "./Wiggle";
 import { lerp } from "../utils/functional";
+import { DogBoneName } from "../types";
 
+const just0 = () => 0;
 const mapBool = (vTrue = 1, vFalse = 0) => bool => bool ? vTrue : vFalse;
 const boolTo01 = mapBool(1,0)
 const isCloseToMouse = (b) => b?.userData.distanceToMouse < 0.01 || b?.children.some((c) => c.userData.distanceToMouse < 0.0025)
-export const EarWiggle: React.FC = () => {
-  const bones = useDogBones(['EarL_0', "EarR_0"])
-  const y0 = useMemo(() => bones.map(b => b?.rotation.y) , bones)
-  const [proximity, setProximity] = useState<number[]>(bones.map(() => 0))
+
+export const EarWiggle: React.FC<{bones: DogBoneName[];}> = ({bones}) => {
+  const boneObjs = useDogBones(bones)
+  const y0 = useMemo(() => boneObjs.map(b => b?.rotation.y) , boneObjs)
+  const [proximity, setProximity] = useState<number[]>(boneObjs.map(just0))
   useFrame(() => {
-    const p1 = bones.map(isCloseToMouse).map(boolTo01)
+    const p1 = boneObjs.map(isCloseToMouse).map(boolTo01)
     setProximity(prev=>prev.map((v,i) => lerp(v,p1[i],0.2)))
   })
   return <>{
-    bones.filter(Boolean).map((bone,i) => <Wiggle key={bone.name} object={bone} axis="y" vOffset={y0[i]} amp={0.2 * proximity[i]} speed={0.7} />)
+    boneObjs.filter(Boolean).map((bone,i) => <Wiggle key={bone.name} object={bone} axis="y" vOffset={y0[i]} amp={0.2 * proximity[i]} speed={0.7} />)
   }</>;
 };
+EarWiggle.defaultProps = {
+  bones: ['EarL_0', "EarR_0"]
+}
