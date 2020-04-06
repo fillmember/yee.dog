8390bdcd0a2210ede43d20f0c8440437131b0f4c
Ten <ten@mutate.space>
2020-04-06 07:43:54 +0200

    fix earWiggle slowdown

--- src/fiber/components/EarWiggle.tsx
+++ src/fiber/components/EarWiggle.tsx
@@ -8,12 +8,11 @@ import { useDogBones } from "../hooks/useDogBone";
 
 const json = (boneName: DogBoneName, n: number) => ({
   name: `wiggle-${boneName}`,
-  duration: 2,
   tracks: [
     {
       type: "number",
       name: `${boneName}.rotation[y]`,
-      times: [0, 1, 2],
+      times: [0, 0.01, 0.02],
       values: arrOf(0.59 * n, 3).map((v, i) => v + (i - 1) * 0.3 * n),
     },
   ],
@@ -23,7 +22,6 @@ const jsonR = json("EarR_0", -1);
 const mapBool = (vTrue = 1, vFalse = 0) => bool => bool ? vTrue : vFalse;
 const boolTo01 = mapBool(1,0)
 const isCloseToMouse = (b) => b?.userData.distanceToMouse < 0.01 || b?.children.some((c) => c.userData.distanceToMouse < 0.0025)
-const boolToScale = () => 100
 const boolToAlpha = mapBool(0.1,0.2)
 
 export const EarWiggle: React.FC = () => {
@@ -32,7 +30,6 @@ export const EarWiggle: React.FC = () => {
   useEffect(() => {
     animations.forEach(({ action }) => {
       action.setLoop(LoopPingPong, Infinity);
-      action.timeScale = boolToScale();
       action.weight = boolTo01(false);
       action.play();
     });
@@ -40,12 +37,9 @@ export const EarWiggle: React.FC = () => {
   useFrame(() => {
     const proximity = bones.map(isCloseToMouse)
     const weights = proximity.map(boolTo01)
-    const scales = proximity.map(boolToScale)
     const alphas = proximity.map(boolToAlpha)
     animations.forEach(({action}, index) => {
       action.weight = lerp( action.weight, weights[index], alphas[index])
-      action.timeScale = lerp( action.timeScale, scales[index], 0.5)
-      console.log(action.timeScale)
     })
   })
   return null;
