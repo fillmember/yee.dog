56df76ddcf91a7009cd1ba566f81ffd38977c623
Ten <ten@mutate.space>
2020-08-27 08:54:30 +0200

    feat: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import Head from "next/head";
 import "../src/css/style.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>Hello!</Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
